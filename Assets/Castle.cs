26da279eb4f0ca8988b27de81118e1e7f8b1a3bc
yu sky <sky9622uiop@naver.com>
2024-11-12 20:42:45 +0900

    캐슬 완성본

--- Assets/Castle.cs
+++ Assets/Castle.cs
@@ -9,6 +9,9 @@ public class Castle : MonoBehaviour
     private float MaxHp;
     private float Damge;
     private Image GuageBar;
+    private SpriteRenderer spriteRenderer;
+    public Sprite destroyedSprite; // 파괴된 성의 스프라이트
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +19,29 @@ public class Castle : MonoBehaviour
         Damge = 1;
         GuageBar = GameObject.Find("HpGuage").GetComponent<Image>();
         GuageBar.fillAmount = 1;
+        spriteRenderer = GetComponent<SpriteRenderer>(); // SpriteRenderer 초기화
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GuageBar.fillAmount <= 0)
+        if (GuageBar.fillAmount <= 0)
         {
-            SceneManager.LoadScene("GameOver");
+            spriteRenderer.sprite = destroyedSprite; // 스프라이트 변경
+            spriteRenderer.transform.localScale = new Vector3(3, 3, 1); // 스프라이트 크기 조정
+            StartCoroutine(DestroyCastle()); // 성 파괴 코루틴 시작
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GuageBar.fillAmount -= Damge / MaxHp;
         Destroy(collision.gameObject);
+    }
+
+    private IEnumerator DestroyCastle()
+    {
+        yield return new WaitForSeconds(2); // 이미지 표시 시간 대기
+        SceneManager.LoadScene("GameOver"); // GameOver 씬 로드
     }
 }
